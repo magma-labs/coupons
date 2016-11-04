7f99240eb2b10e8eef4487d0387b3eff3222e99a
João Loff <jfloff@gmail.com>
2016-11-04 14:20:30 +0000

    Add admin to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Coupons::Engine.routes.draw do
-  patch '/coupons', to: 'coupons#batch'
   get '/coupons/apply', to: 'coupons#apply', as: 'apply_coupon', format: 'json'
 
-  resources :coupons do
-    get :remove, on: :member
-    get :duplicate, on: :member
+  scope '/admin' do
+    patch '/coupons', to: 'coupons#batch'
+    resources :coupons do
+      get :remove, on: :member
+      get :duplicate, on: :member
+    end
   end
 end
