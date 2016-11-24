424609a1009f846215927f6fd5ded504adb0e0af
Julian Hernandez <julian.hernandez@magmalabs.io>
2016-11-23 19:18:39 -0600

    Engine '/admin' scope in routes removed.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Coupons::Engine.routes.draw do
   get '/coupons/apply', to: 'coupons#apply', as: 'apply_coupon', format: 'json'
+  patch '/coupons', to: 'coupons#batch'
 
-  scope '/admin' do
-    patch '/coupons', to: 'coupons#batch'
-    resources :coupons do
-      get :remove, on: :member
-      get :duplicate, on: :member
-    end
+  resources :coupons do
+    get :remove, on: :member
+    get :duplicate, on: :member
   end
 end
