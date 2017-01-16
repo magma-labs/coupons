f0ec0a39ca12e320238e2b7e0507ff75ea8fe639
Julian Hernandez <julian.hernandez@magmalabs.io>
2017-01-16 05:45:32 -0600

    Logic for limit per user added

--- lib/coupons/finders/user_availability.rb
+++ lib/coupons/finders/user_availability.rb
@@ -1,22 +1,45 @@
 module Coupons
   module Finders
+    # Possible return status: found, not_found, limit_exceeded
     UserAvailability = proc do |code, options = {}|
       coupons = Models::Coupon.where(code: code).select do |coupon|
-        if coupon.redemption_limit_user.zero?
-          coupon.redeemable?
-        else
-          user_id = options[:user_id]
+        coupon.started? && !coupon.expired?
+      end
 
-          if user_id.blank?
-            false
-          else
-            user_id = user_id.to_s.strip
-            coupon.redeemable? user_id
-          end
+      selected_coupon = coupons.first
+      options[:status] = 'not_found'
+      terminate = false
+
+      terminate = true if selected_coupon.nil?
+
+      if !terminate && selected_coupon.redemption_limit_user.zero?
+        if selected_coupon.redeemable?
+          options[:status] = 'found'
+        else
+          selected_coupon = nil
+        end
+
+        terminate = true
+      end
+
+      if !terminate
+        user_id = options[:user_id]
+
+        if user_id.blank?
+          selected_coupon = nil
+          terminate = true
         end
       end
 
-      coupons.first
+      if !terminate && !selected_coupon.redeemable?(user_id)
+        options[:status] = 'limit_exceeded'
+        selected_coupon = nil
+        terminate = true
+      end
+
+      options[:status] = 'found' if !terminate
+
+      selected_coupon
     end
   end
 end
